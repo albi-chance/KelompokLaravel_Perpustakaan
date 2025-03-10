ab2168d89b5ea76170ac7e673f1a94bf1a3fda79
RulZz <kingofmakmuhamadsyahrul@gmail.com>
2025-03-10 07:41:11 +0700

    perunahan navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,7 +78,7 @@
                     <a class="nav-link {{ request()->is('buku') ? 'active' : '' }}" href="#">Buku</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->is('tentang') ? 'active' : '' }}" href="#">Tentang</a>
+                    <a class="nav-link {{ request()->is('tentang') ? 'active' : '' }}" href="#">Genre</a>
                 </li>
             </ul>
         </div>
