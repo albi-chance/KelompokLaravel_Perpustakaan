98f0ef6ab7b8a4cd6f15f3a923df99889960ed43
RulZz <kingofmakmuhamadsyahrul@gmail.com>
2025-03-11 08:06:30 +0700

    nambahin route

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
-});
-Route::get('/book', function () {
-    return view('book');
-});
+//Bagian member
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
+//bagian admin
+Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
+
+//bagian yang belum login
+Route::get('/index', [IndexControlle::class, 'home'])->name('index');
+
+// Cia kamu yang ngertikan yang nyambungin route tuh aku ga paham jadi kamu aja yah😁😁
